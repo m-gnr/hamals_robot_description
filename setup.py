709a17fa91688b4259d561e7b6b946e0ee1d19d7
m_gnr <m_gnr@icloud.com>
2026-02-09 17:54:18 +0300

    Finalize robot description: geometry, wheels, casters, lidar mounting

--- setup.py
+++ setup.py
@@ -7,27 +7,32 @@ package_name = 'hamals_robot_description'
 setup(
     name=package_name,
     version='0.0.0',
+
     packages=[],
+
     data_files=[
-        # ament index
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
 
-        # package.xml
-        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name,
+            ['package.xml']),
 
-        # URDF / XACRO files
         (os.path.join('share', package_name, 'urdf'),
             glob('urdf/*.xacro')),
 
-        # launch dosyaları varsa
+        (os.path.join('share', package_name, 'config'),
+            glob('config/*.yaml')),
+
         (os.path.join('share', package_name, 'launch'),
             glob('launch/*.py')),
     ],
+
     install_requires=['setuptools'],
     zip_safe=True,
+
     maintainer='m_gnr',
     maintainer_email='m_gnr@icloud.com',
+
     description='Hamals robot description (URDF/Xacro)',
     license='MIT',
 )
