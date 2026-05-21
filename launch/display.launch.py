a6d35b19b9658dbf1922925979ca74780f29428e
m-gnr <m_gnr@icloud.com>
2026-05-21 21:10:52 +0300

    Refactor robot description launch and xacro structure

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -1,36 +1,47 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.substitutions import Command
-from ament_index_python.packages import get_package_share_directory
-import os
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution
+
 
 def generate_launch_description():
+    package_share = FindPackageShare("hamals_robot_description")
 
-    pkg_path = get_package_share_directory('hamals_robot_description')
-    xacro_file = os.path.join(pkg_path, 'urdf', 'hamals_robot.urdf.xacro')
+    robot_description_file = PathJoinSubstitution([
+        package_share,
+        "urdf",
+        "hamals_robot.urdf.xacro"
+    ])
 
-    robot_description = Command(['xacro ', xacro_file])
+    robot_description = {
+        "robot_description": Command([
+            "xacro ",
+            robot_description_file
+        ])
+    }
+
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[robot_description],
+        output="screen"
+    )
+
+    joint_state_publisher_gui = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui",
+        output="screen"
+    )
+
+    rviz = Node(
+        package="rviz2",
+        executable="rviz2",
+        output="screen"
+    )
 
     return LaunchDescription([
-
-        Node(
-            package='joint_state_publisher',
-            executable='joint_state_publisher',
-            output='screen'
-        ),
-
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            output='screen',
-            parameters=[{
-                'robot_description': robot_description
-            }]
-        ),
-
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            output='screen'
-        )
+        joint_state_publisher_gui,
+        robot_state_publisher,
+        rviz
     ])
--- setup.py
+++ setup.py
@@ -1,38 +1,33 @@
 from setuptools import setup
-import os
 from glob import glob
+import os
 
-package_name = 'hamals_robot_description'
+package_name = "hamals_robot_description"
 
 setup(
     name=package_name,
-    version='0.0.0',
-
+    version="0.0.1",
     packages=[],
-
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-
-        ('share/' + package_name,
-            ['package.xml']),
-
-        (os.path.join('share', package_name, 'urdf'),
-            glob('urdf/*.xacro')),
-
-        (os.path.join('share', package_name, 'config'),
-            glob('config/*.yaml')),
-
-        (os.path.join('share', package_name, 'launch'),
-            glob('launch/*.py')),
+        ("share/ament_index/resource_index/packages", [
+            os.path.join("resource", package_name)
+        ]),
+        ("share/" + package_name, [
+            "package.xml"
+        ]),
+        (os.path.join("share", package_name, "config"), glob("config/*.yaml")),
+        (os.path.join("share", package_name, "urdf"), glob("urdf/*.xacro")),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.py")),
+        (os.path.join("share", package_name, "rviz"), glob("rviz/*.rviz")),
     ],
-
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-
-    maintainer='m_gnr',
-    maintainer_email='m_gnr@icloud.com',
-
-    description='Hamals robot description (URDF/Xacro)',
-    license='MIT',
+    maintainer="Murat Güner",
+    maintainer_email="m_gnr@icloud.com",
+    description="HAMALS forklift AGV robot description package",
+    license="MIT",
+    entry_points={
+        "console_scripts": [
+        ],
+    },
 )
